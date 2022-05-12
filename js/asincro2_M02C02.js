aef163010233bf270f46d18b81708cc42d485683
Sandra Valenzuela <sandravalenzuela.fi@gmail.com>
2022-05-12 16:25:56 -0400

    cambios 1

--- js/asincro2_M02C02.js
+++ js/asincro2_M02C02.js
@@ -1,27 +1,30 @@
 var numpar=2;
 
 
-/*
+// ejercicio  Par o Impar
 var numpar=prompt("Ingrese un  2 o 3 ");
 if (numpar==2){
     alert("el número que ingreso es par")    
 } else {
     alert("el número que ingreso es impar");
 
-}   */
+}  
+
+//---------------------------
+// Ejercicio Edad de Jubilarse
 var sexo="Hombre";
 var sexo ="Mujer";
-var edadMuj =60;
-var edadHom =65;
+var edadM =60;
+var edadH =65;
 prompt("ingrese Mujer o Hombre ");
 prompt("ingrese edad");
-if((edadMuj>=60) && sexo=="Mujer"){
+if(edadM>=60 && sexo=="Mujer"){
     alert (" Ud. está en edad de jubilarse");
-}else if ((edadMuj <=59) && sexo=="Mujer"){
+}else if (edadM<60 && sexo=="Mujer"){
     alert ("Ud. No está en edad de jubilarse");
-}else if(sexo=="Hombre" && (edadHom>=65)){
+}else if(sexo=="Hombre" && edadH>=65){
     alert("usted esta en edad de jubilarse");
-}else if(sexo== "Hombre" && edadHom<=64)
+}else if(sexo== "Hombre" && edadH<=64)
     alert("Ud. No esta en edad de jubilarse");
 
 
--- js/asincro3_M02C03.js
+++ js/asincro3_M02C03.js
@@ -1,3 +1,45 @@
-var numero = Number (prompt("Ingrese un numero"));
-var cuadrado = numero*numero;
-alert ("el cuadrado de " +  "es " + cuadrado);
+// ejercicio Multiplo de 3 y 5
+    function multiplo(Valor, multi){
+        var respuesta= Valor % multi;
+        if (respuesta ==0) 
+            return true;
+        else
+            return false
+}
+function multiple(){
+    var min = 0;
+    for(var i =100; i>min; i--){
+        if(multiplo(i,3) && multiplo(1,5)){
+            console.log(i,"FizzBuzz")
+        }else{
+            if (multiplo (i,3)){
+                console.log(i,"Fizz")
+            }else{
+                if (multiplo (i,5)){
+                    console.log(i,"Buzz")
+                }else{
+                    console.log(i,"terminó")
+                }
+            }
+        }
+    }
+}
+
+//------------------------------------------------------------------------
+//ejercicio de piedra, Papel o Tijera
+ prompt("Ingrese: Piedra, Papel o Tijera");
+ var piedra= 0;
+ var papel= 1;
+ var tijera= 2;
+ if (piedra==0){
+      console.log("Piedra gana a tijera")
+    }else if (papel== 1){
+        console.log("papel le gana a piedra")
+        }else if(tijera==2){
+            console.log("termino juego")
+}
+
+    
+
+
+    
